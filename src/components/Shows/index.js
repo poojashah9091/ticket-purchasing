2a1e6a552b40576702f03dafa5fcf8c287dd8fff
Pooja Shah <poojashah9091@gmail.com>
2023-04-14 19:54:04 -0400

    Shows list compoenent

--- src/components/Shows/index.js
+++ src/components/Shows/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getShowsList } from "../../data/showsSlice";
+import ShowCard from "../ShowCard";
+import "./style.scss";
 
 const Shows = () =>{
+
+    const dispatch = useDispatch();
+    const showsList = useSelector(store=>store.shows.allShowsList);
+
+    useEffect(()=>{
+        dispatch(getShowsList());
+    },[]);
+
     return (
-        <div>
-            Shows
+        <div className="show_list_container">
+            {
+                showsList.map(show=>{
+                    return(
+                        <ShowCard 
+                            key={show.title}
+                            title={show.title}
+                            posterURL={show.posterURL}
+                            price={show.price}
+                            showDate={show.date}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
